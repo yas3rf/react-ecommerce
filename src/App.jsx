1f5f2be7de0a8cc32312118a73461c20475098d0
yas3rf <faqiriyaser@gmai.com>
2022-08-24 00:18:39 +0530

    first commit of ECOMMERCE front-end

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+
+function App() {
+  return <div className="App">
+    <Register/>
+  </div>;
+}
+
+export default App;
